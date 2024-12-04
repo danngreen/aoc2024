2bba6fdbd9ddd869b371a6ae64decb472addb795
Dan Green <danngreen1@gmail.com>
2024-12-03 17:13:58 -0800

    WIP 02

--- 02/main.cc
+++ 02/main.cc
@@ -1,7 +1,31 @@
 #include "../common/fileparse.hh"
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+
+bool row_is_safe(std::span<int> vals) {
+	auto diffs = vals | std::views::adjacent_transform<2>([](auto &&x, auto &&y) { return x - y; });
+
+	auto passes = std::ranges::fold_left(
+		diffs, 0, [](int sum, int x) { return sum + (std::abs(x) <= 3 && std::abs(x) >= 1) ? 1 : 0; });
+	// auto passes = diffs | std::views::acc([](auto &&x) { return std::abs(x) <= 3 && std::abs(x) >= 1; });
+
+	return passes == vals.size();
+}
 
 int main(int argc, char *argv[]) {
-	auto input = read_file(argv[1]);
+	auto rows = parse_rows(argv[1]);
 
+	std::cout << "Found " << rows.size() << " rows\n";
+
+	size_t num_safe = 0;
+	for (auto &row : rows) {
+		auto safe = row_is_safe(row);
+		num_safe += safe ? 1 : 0;
+
+		for (auto i : row)
+			std::cout << i << " ";
+		std::cout << " => " << safe << "\n";
+	}
 	return 0;
 }
