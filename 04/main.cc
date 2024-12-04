a1ccda767f1d6a92ac867f8e894681b792cd437e
Dan Green <danngreen1@gmail.com>
2024-12-04 09:16:47 -0800

    04 part 2 (brute force)

--- 04/main.cc
+++ 04/main.cc
@@ -44,66 +44,88 @@ static_assert(shift_and_pad("ABC", 1, 6, ' ') == " ABC  ");
 static_assert(shift_and_pad("ABC", 2, 6, ' ') == "  ABC ");
 static_assert(shift_and_pad("ABC", 3, 6, ' ') == "   ABC");
 
-int main(int argc, char *argv[]) {
+std::pair<int, int> find_in_rows(auto &&rows) {
 	auto num_right = 0;
 	auto num_left = 0;
-	auto num_down = 0;
-	auto num_up = 0;
-	auto num_upright = 0;
-	auto num_downleft = 0;
-	auto num_upleft = 0;
-	auto num_downright = 0;
-
-	auto rows = parse_lines(argv[1]);
 
 	for (auto const &row : rows) {
 		num_right += count_found(row, "XMAS");
 		num_left += count_found(row, "SAMX");
 	}
 
-	{
-		auto cols = transpose_rows_to_cols(rows);
-		for (auto const &row : cols) {
-			num_down += count_found(row, "XMAS");
-			num_up += count_found(row, "SAMX");
+	return {num_right, num_left};
+}
+
+unsigned find_xs(std::vector<std::string> grid) {
+
+	auto numrows = grid.size();
+	auto numcols = grid.front().length();
+
+	unsigned found = 0;
+
+	for (unsigned y = 1; y < numrows - 1; y++) {
+		for (unsigned x = 1; x < numcols - 1; x++) {
+			if (grid[y][x] == 'A') {
+				//M M  M S  S S  S M
+				// A    A    A    A
+				//S S  M S  M M  S M
+				if (grid[y - 1][x - 1] == 'M' && grid[y + 1][x + 1] == 'S') {
+					if (grid[y - 1][x + 1] == 'M' && grid[y + 1][x - 1] == 'S') {
+						found++;
+					}
+					if (grid[y - 1][x + 1] == 'S' && grid[y + 1][x - 1] == 'M') {
+						found++;
+					}
+				}
+				if (grid[y - 1][x - 1] == 'S' && grid[y + 1][x + 1] == 'M') {
+					if (grid[y - 1][x + 1] == 'M' && grid[y + 1][x - 1] == 'S') {
+						found++;
+					}
+					if (grid[y - 1][x + 1] == 'S' && grid[y + 1][x - 1] == 'M') {
+						found++;
+					}
+				}
+			}
 		}
 	}
+	return found;
+}
+
+int main(int argc, char *argv[]) {
+
+	auto rows = parse_lines(argv[1]);
+
+	auto [num_right, num_left] = find_in_rows(rows);
+
+	auto cols = transpose_rows_to_cols(rows);
+	auto [num_down, num_up] = find_in_rows(cols);
 
 	// Shift row N to the right N positions
-	{
-		std::vector<std::string> shift_r;
-		for (unsigned i = 0; auto const &row : rows) {
-			shift_r.push_back(shift_and_pad(row, i, row.size() * 2));
-			i++;
-		}
-		auto cols_shift_r = transpose_rows_to_cols(shift_r);
-		for (auto const &row : cols_shift_r) {
-			num_downleft += count_found(row, "XMAS");
-			num_upright += count_found(row, "SAMX");
-		}
+	std::vector<std::string> shift_r;
+	for (unsigned i = 0; auto const &row : rows) {
+		shift_r.push_back(shift_and_pad(row, i, row.size() * 2));
+		i++;
 	}
+	auto cols_shift_r = transpose_rows_to_cols(shift_r);
+	auto [num_downleft, num_upright] = find_in_rows(cols_shift_r);
 
 	// Shift row N to the left N positions
-	{
-		std::vector<std::string> shift_l;
-		for (unsigned i = 0; auto const &row : rows) {
-			shift_l.push_back(shift_and_pad(row, rows.size() - i, row.size() * 2));
-			i++;
-		}
-		auto cols_shift_l = transpose_rows_to_cols(shift_l);
-		for (auto const &row : cols_shift_l) {
-			num_downright += count_found(row, "XMAS");
-			num_upleft += count_found(row, "SAMX");
-		}
+	std::vector<std::string> shift_l;
+	for (unsigned i = 0; auto const &row : rows) {
+		shift_l.push_back(shift_and_pad(row, rows.size() - i, row.size() * 2));
+		i++;
 	}
+	auto cols_shift_l = transpose_rows_to_cols(shift_l);
+	auto [num_downright, num_upleft] = find_in_rows(cols_shift_l);
 
 	auto total = num_right + num_left + num_up + num_down + num_upleft + num_upright + num_downleft + num_downright;
-	std::cout << num_right << " " << num_left << " " << num_up << " " << num_down << " " << num_upleft << " "
-			  << num_upright << " " << num_downleft << " " << num_downright;
 
 	std::cout << "Part 1: total XMAS found: " << total << "\n";
+	//2562
 
 	// brute force find X-MAS:
+	auto num_xs = find_xs(rows);
+	std::cout << "Part 2: total X-MAS found: " << num_xs << "\n";
 
 	return 0;
 }
