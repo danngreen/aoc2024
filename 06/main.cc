fb90c2c20d097d6fa17f2aa3b2650d08e44ef10c
Dan Green <danngreen1@gmail.com>
2024-12-05 22:57:45 -0800

    05 part 2 WIP

--- 06/main.cc
+++ 06/main.cc
@@ -8,8 +8,6 @@ struct Guard {
 	int x;
 	int y;
 	char dir;
-	// int next_x;
-	// int next_y;
 };
 
 constexpr std::string_view Dirs = "^>v<";
@@ -25,6 +23,11 @@ constexpr std::pair<int, int> next(char dir) {
 		return {1, 0};
 	} else
 		return {0, 0};
+}
+
+constexpr std::pair<int, int> next(Guard guard) {
+	auto [dx, dy] = next(guard.dir);
+	return {guard.x + dx, guard.y + dy};
 }
 
 constexpr char rotate(char dir) {
@@ -48,35 +51,90 @@ Guard find_guard(std::vector<std::string> const &grid) {
 	return {};
 }
 
-bool step(std::vector<std::string> &grid, Guard &guard) {
+//actually `obstacles` is a vec of places where we would rotate if there was an obstacle in the next spot
+std::vector<std::pair<int, int>> obstacles;
 
-	auto [dx, dy] = next(guard.dir);
-	auto test_x = guard.x + dx;
-	auto test_y = guard.y + dy;
+bool in_bounds(int x, int y, auto const &grid) {
+	auto in_bounds_x = std::clamp<int>(x, 0, grid.front().size() - 1) == x;
+	auto in_bounds_y = std::clamp<int>(y, 0, grid.size() - 1) == y;
+	return in_bounds_y && in_bounds_x;
+}
 
-	auto in_bounds_x = std::clamp<int>(test_x, 0, grid.front().size() - 1) == test_x;
-	auto in_bounds_y = std::clamp<int>(test_y, 0, grid.front().size() - 1) == test_y;
-	if (!in_bounds_x || !in_bounds_y) {
+bool obstacle_would_loop(std::vector<std::string> grid, Guard guard);
+
+bool step(std::vector<std::string> &grid, Guard &guard, bool check_obs = true) {
+
+	auto [test_x, test_y] = next(guard);
+
+	if (!in_bounds(test_x, test_y, grid)) {
 		grid[guard.y][guard.x] = guard.dir;
 		return false;
 	}
 
 	if (grid[test_y][test_x] != '#') {
-		grid[guard.y][guard.x] = guard.dir;
+		if (check_obs) {
+			Guard test_guard{test_x, test_y, rotate(guard.dir)};
+			if (obstacle_would_loop(grid, test_guard)) {
+				auto [obstx, obsty] = next(Guard{test_x, test_y, guard.dir});
+				obstacles.push_back({obstx, obsty});
+				std::cout << "______________________" << "\n";
+				std::cout << obstx << " " << obsty << "\n";
+				auto c = grid[obsty][obstx];
+				grid[obsty][obstx] = 'O';
+				print_grid(grid);
+				grid[obsty][obstx] = c;
+			}
+		}
 		guard.x = test_x;
 		guard.y = test_y;
+		grid[guard.y][guard.x] = guard.dir;
 	} else {
 		guard.dir = rotate(guard.dir);
+		grid[guard.y][guard.x] = guard.dir;
 	}
-	grid[guard.y][guard.x] = guard.dir;
 
 	return true;
 }
 
+bool obstacle_would_loop(std::vector<std::string> grid, Guard guard) {
+	grid[guard.y][guard.x] = guard.dir;
+
+	std::cout << "############################" << "\n";
+	while (true) {
+		print_grid(grid);
+		auto [dx, dy] = next(guard.dir);
+		auto test_x = guard.x + dx;
+		auto test_y = guard.y + dy;
+
+		if (!in_bounds(test_x, test_y, grid)) {
+			std::cout << "No loop\n";
+			return false;
+		}
+
+		if (grid[test_y][test_x] == guard.dir) {
+			std::cout << "Loop\n";
+			return true;
+		}
+
+		if (grid[test_y][test_x] != '#') {
+			guard.x = test_x;
+			guard.y = test_y;
+			grid[guard.y][guard.x] = guard.dir;
+		} else {
+			guard.dir = rotate(guard.dir);
+		}
+	}
+
+	return false;
+}
+
 size_t count_xs(std::vector<std::string> const &grid) {
 	return std::accumulate(grid.begin(), grid.end(), 0u, [](size_t sum, std::string const &line) {
-		return sum + std::count(line.begin(), line.end(), '^') + std::count(line.begin(), line.end(), '<') +
-			   std::count(line.begin(), line.end(), '>') + std::count(line.begin(), line.end(), 'v');
+		return sum										 //
+			 + std::count(line.begin(), line.end(), '^') //
+			 + std::count(line.begin(), line.end(), '<') //
+			 + std::count(line.begin(), line.end(), '>') //
+			 + std::count(line.begin(), line.end(), 'v');
 	});
 }
 
@@ -95,6 +153,9 @@ int main(int argc, char *argv[]) {
 
 	std::cout << "Part 1: " << num_xs << "\n";
 	//4982
+
+	std::cout << "Part 2: " << obstacles.size() << "\n";
+	//<1687
 
 	return 0;
 }
