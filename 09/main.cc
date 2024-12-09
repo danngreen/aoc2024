d24bcabe096f14dc7f6d8b262571a680361405a2
Dan Green <danngreen1@gmail.com>
2024-12-09 00:03:33 -0800

    09 part 2, and cleanup part 1

--- 09/main.cc
+++ 09/main.cc
@@ -1,74 +1,85 @@
 #include <fstream>
 #include <iostream>
-// #include <map>
-// #include <set>
-// #include <span>
-// #include <vector>
 #include <numeric>
 #include <string>
+#include <vector>
 
 void print_blocks(std::vector<int> const &blocks);
+
+struct Entry {
+	int start;
+	int size;
+};
+
+// filetab and spacetab are used for part 2
+std::vector<Entry> filetab;
+std::vector<Entry> spacetab;
+
+// blocks is used for part 1
 
 std::vector<int> parse_to_blocks(std::string filename) {
 	std::ifstream ifs(std::string{filename});
 	std::string line(std::istreambuf_iterator<char>{ifs}, {});
 	line = line.substr(0, line.length() - 1); //trim ending newline
-	// std::cout << line << "|\n";
 
 	std::vector<int> blocks;
-	// blocks.reserve(?);
-	unsigned fileid = 0;
+
 	bool isfile = true;
 	for (auto c : line) {
-		size_t num = c - '0';
+		size_t size = c - '0';
+
 		if (isfile) {
-			blocks.insert(blocks.end(), num, fileid);
-			fileid++;
+			auto fileid = filetab.size();
+			filetab.push_back(Entry(blocks.size(), size));
+			blocks.insert(blocks.end(), size, fileid);
 		} else {
-			blocks.insert(blocks.end(), num, -1);
+			spacetab.push_back(Entry(blocks.size(), size));
+			blocks.insert(blocks.end(), size, -1);
 		}
+
 		isfile = !isfile;
 	}
 
 	return blocks;
 }
 
-// find_last_not(std::vector<int> &blocks) {
-// }
-
 void compact(std::vector<int> &blocks) {
-	size_t end = 0;
-	size_t beg = blocks.size();
-
-	auto it_free = blocks.begin();
-	auto it_file = blocks.rbegin();
+	auto next_free = blocks.begin();
+	auto next_file = blocks.rbegin();
 
 	while (true) {
-		auto next_free = std::find(it_free, blocks.end(), -1);
-		auto next_file = std::find_if(it_file, blocks.rend(), [](auto &el) { return el >= 0; });
+		next_free = std::find_if(next_free, blocks.end(), [](auto &el) { return el < 0; });
+		next_file = std::find_if(next_file, blocks.rend(), [](auto &el) { return el >= 0; });
 
-		auto next_free_idx = std::distance(blocks.begin(), next_free);
-		auto next_file_idx = -std::distance(blocks.rend(), next_file) - 1; //??? -1 ??
-
-		if (next_free_idx >= next_file_idx) {
-			std::cout << "done: " << next_free_idx << " >= " << next_file_idx << "\n";
+		if (std::make_reverse_iterator(next_free) <= next_file)
 			return;
-		}
 
-		// std::cout << "[" << next_free_idx << "]<->[" << next_file_idx << "]" << "\n";
-		// std::swap(next_free, next_file);
-		// std::swap(blocks[next_free_idx], blocks[next_file_idx]);
-		auto t = blocks[next_free_idx];
-		blocks[next_free_idx] = blocks[next_file_idx];
-		blocks[next_file_idx] = t;
+		std::swap(*next_free, *next_file);
 
-		it_free = next_free + 1;
-		it_file = next_file + 1;
+		next_free++;
+		next_file++;
+
 		// print_blocks(blocks);
 	}
 }
 
-long long checksum(std::vector<int> &blocks) {
+void compact_whole(std::vector<int> &blocks) {
+	// try to move each file, starting with the last
+	for (size_t fileid = filetab.size() - 1; fileid > 0; fileid--) {
+		auto &file = filetab[fileid];
+
+		auto free = std::find_if(spacetab.begin(), spacetab.end(), [&file](Entry const &e) {
+			return e.size >= file.size && e.start < file.start;
+		});
+
+		if (free != spacetab.end()) {
+			file.start = free->start;
+			*free = {.start = free->start + file.size, .size = free->size - file.size};
+		}
+	}
+}
+
+long long checksum_blocks(std::vector<int> &blocks) {
 	int idx = -1;
 	return std::accumulate(blocks.begin(), blocks.end(), 0LL, [&idx](long long sum, int el) {
 		idx++;
@@ -80,28 +91,55 @@ long long checksum(std::vector<int> &blocks) {
 	});
 }
 
+long long checksum_filetab(std::vector<Entry> const &filetab) {
+	long long sum = 0;
+
+	for (unsigned fileid = 0u; auto file : filetab) {
+		for (unsigned i = file.start; i < file.start + file.size; i++) {
+			sum += i * fileid;
+		}
+		fileid++;
+	}
+
+	return sum;
+}
+
 int main(int argc, char *argv[]) {
-	auto blocks = parse_to_blocks(argv[1]);
-	// print_blocks(blocks);
-	compact(blocks);
-	// print_blocks(blocks);
+	const auto blocks = parse_to_blocks(argv[1]);
 
-	auto chk = checksum(blocks);
+	{
+		auto blocks1 = blocks;
 
-	std::cout << "Part 1: " << chk << "\n";
-	// std::cout << "Part 2: " << antinodes.size() << "\n";
+		compact(blocks1);
+
+		auto chk = checksum_blocks(blocks1);
+
+		std::cout << "Part 1: " << chk << "\n";
+		// 6334655979668
+	}
+
+	{
+		auto blocks2 = blocks;
+
+		compact_whole(blocks2);
+
+		auto chk2 = checksum_filetab(filetab);
+
+		std::cout << "Part 2: " << chk2 << "\n";
+		//   6349492251099
+	}
+
 	return 0;
 }
 
 void print_blocks(std::vector<int> const &blocks) {
 
 	for (auto e : blocks) {
-		// std::cout << std::setfill('0') << std::setw(4);
 
-		if (e == -1)
-			std::cout << "."; //"____ ";
+		if (e < 0)
+			std::cout << e << " ";
 		else
-			std::cout << e << "";
+			std::cout << " " << e << " ";
 	}
 	std::cout << "\n";
 }
