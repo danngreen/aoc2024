45669582e5a9158f57ea56f6127699e813589aed
Dan Green <danngreen1@gmail.com>
2024-12-11 23:22:44 -0800

    Start 12

---
+++ 12/main.cc
@@ -0,0 +1,20 @@
+#include "../common/fileparse.hh"
+// #include <fstream>
+// #include <iostream>
+// #include <map>
+// #include <numeric>
+// #include <ranges>
+// #include <string>
+// #include <vector>
+
+// void print_map(std:: stones);
+
+int main(int argc, char *argv[]) {
+	auto map = parse_lines(argv[1]);
+
+	// std::cout << "Part 1: " << total << "\n";
+
+	// std::cout << "Part 2: " << total2 << "\n";
+
+	return 0;
+}
