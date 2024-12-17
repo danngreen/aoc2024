fc3f5c217b6fa59065d242642f1c000bb410ab79
Dan Green <danngreen1@gmail.com>
2024-12-16 21:17:32 -0800

    Cleanup 15

--- 15/main.cc
+++ 15/main.cc
@@ -20,26 +20,23 @@ struct Vec {
 	}
 };
 
-enum Dir { E, S, W, N, None };
+enum Dir { E, S, W, N };
 constexpr std::array<Vec, 4> Dirs{{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}};
 
 constexpr Dir opposing(Dir b) {
 	return Dir(((int)b + 2) % 4);
 }
-constexpr bool parallel(Dir a, Dir b) {
-	return (a == b) || (a == opposing(b));
-}
-
 struct State {
 	Vec pos;
 	Dir dir;
 	unsigned score{};
 };
 
-std::vector<unsigned> run_paths(State start, Vec end) {
-	std::vector<std::vector<Dir>> visited(input.front().size(), std::vector<Dir>(input.size(), Dir::None));
+std::vector<int> run_paths(State start, Vec end) {
+	std::vector<std::vector<unsigned>> visited(input.front().size(), std::vector<unsigned>(input.size(), 0x7FFFFFFF));
+	visited[start.pos.y][start.pos.x] = -1;
 
-	std::vector<unsigned> scores{};
+	std::vector<int> scores{};
 
 	std::deque<State> branches;
 	branches.push_front(start);
@@ -48,36 +45,33 @@ std::vector<unsigned> run_paths(State start, Vec end) {
 		auto cur = branches.front();
 		branches.pop_front();
 
-		std::cout << "At " << cur.pos.x << "," << cur.pos.y << ": " << cur.dir << " (" << cur.score << ")\n";
+		std::println("At {},{}: {} ({})", cur.pos.x, cur.pos.y, std::to_underlying(cur.dir), cur.score);
 
 		if (cur.pos == end) {
-			std::cout << "End!\n";
+			std::println("End!");
 			scores.push_back(cur.score);
 			continue;
 		}
 
-		// Allow visiting the same tile twice if we are heading in cross direction
-		// TODO: would it make sense to allow a lower-score state to visit an already visited tile?
-		if (parallel(visited[cur.pos.y][cur.pos.x], cur.dir))
+		// If we've already been to this tile with a lower score, the abort this path
+		if (visited[cur.pos.y][cur.pos.x] <= cur.score)
 			continue;
 
-		visited[cur.pos.y][cur.pos.x] = cur.dir;
+		visited[cur.pos.y][cur.pos.x] = cur.score;
 
-		for (auto dir : {E, S, W, N}) {
-			// Don't allow U-turns
-			if (dir == opposing(cur.dir))
-				continue;
-
-			auto next_pos = cur.pos + Dirs[dir];
+		// Check straight, +90, -90
+		for (auto dir : {0, 1, 3}) {
+			auto new_dir = (cur.dir + dir) % 4;
+			auto next_pos = cur.pos + Dirs[new_dir];
 
 			if (input[next_pos.y][next_pos.x] != '#') {
 				// Prioritize staying on the current path
 				// This weeds out an edge case where we go the "long" way
 				// around a loop
-				if (dir == cur.dir) {
-					branches.push_front(State{next_pos, dir, cur.score + 1});
+				if (new_dir == cur.dir) {
+					branches.push_back(State{next_pos, (Dir)new_dir, cur.score + 1});
 				} else {
-					branches.push_back(State{next_pos, dir, cur.score + 1001});
+					branches.push_back(State{next_pos, (Dir)new_dir, cur.score + 1001});
 				}
 			}
 		}
@@ -91,7 +85,10 @@ int main(int argc, char *argv[]) {
 
 	auto score = run_paths(start, end);
 
-	std::cout << "Part 1: " << *std::ranges::min_element(score) << "\n";
+	if (score.size() > 0)
+		std::println("Part 1: {}", *std::ranges::min_element(score));
+	else
+		std::println("No scores");
 	// 92432
 
 	// std::cout << "Part 2: " << *std::ranges::min_element(score) << "\n";
